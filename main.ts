49cdc7c57b9631935b90eb01bd1fb9c48086fc15
Tobey <gronadebob@gmail.com>
2022-10-28 11:02:06 +1000

    The Big Bang

--- main.ts
+++ main.ts
@@ -1,137 +1,116 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MarkdownView, Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+let mentions: HTMLElement | null = null;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+export default class AB extends Plugin
+{
+	getLinkedMentions(currFilePath: string)
+	{
+		/*
+			This method is taken from https://github.com/dalcantara7/obsidian-auto-moc
+		*/
+		const allFiles = this.app.metadataCache.resolvedLinks;
+		let linkedMentions: Array<string> = [];
+		Object.keys(allFiles).forEach((key) => {
+			if (currFilePath in allFiles[key])
+			{
+				linkedMentions.push(key);
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+
+		return linkedMentions.sort();
+	}
+
+	pathToURL (path: String)
+	{
+		/*
+			Converts a path (folder/subfolder/file.md) to an Obsidian URL
+			path (String) : the path to be converted to an Obsidian URL
+		*/
+
+		// Get vault name and create the URL "header"
+		let name = app.vault.getName();
+		let header = "obsidian://open?vault=" + name + "&file=";
+
+		// Replace spaces and slashes with their Obsidian URL counterpart
+		path = path.replace(/ /g, "%20");
+		path = path.replace(/\//g, "%2F");
+		path = path.replace(".md", "");
+
+		// Create the final URL
+		let url = header + path;
+		
+		return url;
+	}
+
+	showLinks () {
+		/*
+			Puts all linked mentions in the status bar
+		*/
+
+		// Get current file
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+
+		if (view != null && view.file.extension === "md")
+		{
+			// Get linked mentions
+			let linkedMentions = this.getLinkedMentions(view.file.path);
+
+			// Clear the shown mentions from the previous file if necessary
+			if (mentions != null)
+			{
+				mentions.remove();
 			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+
+			// Add a status bar item with mentions class
+			mentions = this.addStatusBarItem();
+			mentions.classList.add("mentions");
+
+			// Only display mentions if necessary
+			if (linkedMentions.length > 0)
+			{
+				// Only display a maximum of 5 mentions
+				let length = linkedMentions.length;
+				if (length > 5)
+				{
+					length = 5;
+				}
+	
+				// Loop through each mention
+				for (let i = 0; i < length; i++)
+				{
+					// Get mention
+					let link = linkedMentions[i];
+	
+					// Remove all file information (folder/file.md --> file)
+					let sanitizedLink = link.replace(/^.*[\\\/]/, '');
+					sanitizedLink = sanitizedLink.replace(".md", '');
+	
+					// Add the mention to the status bar
+					mentions.createEl("a", { text: sanitizedLink, href: this.pathToURL(link), cls: "mention" });
+
+					// Add a space as long as it isn't the final mention
+					if (i != length - 1)
+					{
+						mentions.createEl("span", { text: "    ", cls: "mention-title" });
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			else // No mentions exist for this file
+			{
+				mentions.createEl("span", { text: "No Mentions...", cls: "mention-title-muted" });
+			}
+		}
 	}
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	async onload()
+	{
+		// Execute showLinks whenever a file is opened
+		this.registerEvent(
+		this.app.workspace.on('file-open', () => 
+			{
+				return this.showLinks();
+			}),
+		);
 	}
 }
